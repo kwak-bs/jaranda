597b6ee4d733601379774f8f085a4a90e1c037ef
kwak-bs <iam-bs@naver.com>
2021-08-20 12:34:34 +0900

    Docs : add index.js utils 폴더에서 한번에 export하기 위해

---
+++ src/Pages/SignUp/utils/index.js
@@ -0,0 +1,12 @@
+export { get_address } from './get_address';
+export { inputCheck } from './inputCheck';
+export {
+  IdNumberOrEng,
+  Id4OrMoreDigits,
+  IdSpecialChar,
+  IdHangul,
+  PwEnglish,
+  PwNumber,
+  PwSpecialChar,
+  Pw8OrMoreDigits,
+} from './partValidation';
