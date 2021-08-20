35b54fe9ca694cae0f5115ea3c896b58fa3bdb0f
kwak-bs <iam-bs@naver.com>
2021-08-20 17:53:47 +0900

    Chore : Add annotation in .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,15 @@
+// .prettierrc.js
 module.exports = {
+  // 코드 한줄이 Maximum 80칸
   printWidth: 80,
+  // 코드 마지막에 세미콜론 존재 유무
   semi: true,
+  // 문자열은 홀따옴표로 설정.
   singleQuote: true,
+  // 들여쓰기 너비는 2칸
   tabWidth: 2,
+  // 객체나 배열 키:값 뒤에 항상 콤마를 붙힘.
   trailingComma: 'all',
+  // 탭의 사용을 금하고 스페이스바 사용으로 대체
   useTabs: false,
 };
